7daba1fd3dc67437e904dd565eedf3d5c1dae5a8
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2024-08-13 15:52:21 -0300

    Added initial upload route

---
+++ src/routes/upload/+page.server.js
@@ -0,0 +1,58 @@
+import { error, fail } from '@sveltejs/kit';
+
+function bail(cookies) {
+    cookies.delete('login', { path: '/' });
+    error(401, 'Unauthorized');
+}
+
+export async function load({ cookies, platform }) {
+    const login = cookies.get('login');
+    console.log(login)
+    if (login === undefined) bail(cookies)
+
+    const token = await platform.env.LOGIN_TOKENS.get(login);
+    console.log(token)
+    if (token === undefined) bail(cookies)
+}
+
+export const actions = {
+    default: async ({ cookies, request, platform }) => {
+        const formData = await request.formData();
+        const secret = formData.get('secret');
+        console.log(secret)
+
+        if (!secret || secret === 'undefined')
+            return fail(400, {
+                error: true,
+                message: 'Secret is missing.'
+            });
+
+        if (secret !== platform.env.SECRET)
+            return fail (401, {
+                error: true,
+                message: 'Secret is not valid.'
+            });
+
+        const token = self.crypto.randomUUID();
+        const timestamp = new Date();
+
+        try {
+            await platform.env.LOGIN_TOKENS.put(token, timestamp.toISOString(), {expirationTtl: 3600});
+            cookies.set('login', token, {
+                path: '/',
+                httpOnly: true,
+                maxAge: 3600,
+                secure: true,
+                sameSite: true
+            });
+        }
+        catch {
+            return fail(500, {
+                error: true,
+                message: 'Failed to set login token.'
+            })
+        }
+        
+        redirect(307, '/upload')
+    }
+}
