7a2ebfbee88bb94264069389e3e1f70294386e60
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2024-08-17 14:43:23 -0300

    Reworked code to drop KV requirement

--- src/routes/api/upload/+server.js
+++ src/routes/api/upload/+server.js
@@ -1,6 +1,87 @@
 import { json } from '@sveltejs/kit';
 
-export async function POST({ request }) {
-    console.log(request.headers)
-    return json({ error: false })
+export async function POST({ request, platform }) {
+    const bucket = platform.env.MAIN_BUCKET
+    const secret = platform.env.SECRET
+
+    const fileName = request.headers.get('x-file-name')
+    const action = request.headers.get('x-upload-action')
+    const token = request.headers.get('authorization')
+
+    if (token !== secret) {
+        return json({ error: true, message: 'Unauthorized'}, { status: 401 })
+    }
+
+    if (fileName == null || fileName === '') {
+        return json(
+            { error: true, message: 'File name not provided'},
+            { status: 400 }
+        )
+    }
+
+    switch (action) {
+        case 'create':
+            try {
+                const mpUpload = await bucket.createMultipartUpload(fileName);
+                return json(
+                    {
+                        key: mpUpload.key,
+                        uploadId: mpUpload.uploadId
+                    }
+                )
+            }
+            catch {
+                return json(
+                    { error: true, message: 'Failed to create bucket upload.' },
+                    { status: 500 }
+                )
+            }
+
+        default:
+            return json(
+                { error: true, message: 'Action not provided'}, 
+                { status: 400 }
+            )
+    }
+}
+
+export async function PUT({ request, platform }) {
+    const bucket = platform.env.MAIN_BUCKET
+    const secret = platform.env.SECRET
+
+    const fileName = request.headers.get('x-file-name')
+    const action = request.headers.get('x-upload-action')
+    const token = request.headers.get('authorization')
+    
+    console.log(token)
+    console.log(secret)
+    if (token !== secret) {
+        return json({ error: true, message: 'Unauthorized'}, { status: 401 })
+    }
+
+    if (fileName == null || fileName === '') {
+        return json(
+            { error: true, message: 'File name not provided'},
+            { status: 400 }
+        )
+    }
+
+    switch (action) {
+        case 'direct':
+            try {
+                console.log(await bucket.put(fileName, request.body));
+                return json({ error: false, message: 'Success' })
+            } catch {
+                return json(
+                    { error: true, message: 'Failed to upload file.' },
+                    { status: 500 }
+                )
+            }
+
+        default:
+            return json(
+                { error: true, message: 'Action not provided'}, 
+                { status: 400 }
+            )
+    }
 }
--- src/routes/upload/+page.server.js
+++ src/routes/upload/+page.server.js
@@ -1,57 +1,31 @@
-import { error, fail, redirect } from '@sveltejs/kit';
-import { v4 as uuid } from 'uuid';
+import { error } from '@sveltejs/kit';
 
 function bail(cookies) {
     cookies.delete('login', { path: '/' });
-    error(401, 'Unauthorized');
+    return error(401, 'Unauthorized');
 }
 
 export async function load({ cookies, platform }) {
-    const login = cookies.get('login');
-    console.log(login)
-    if (login === undefined) bail(cookies)
+    const secret = platform.env.SECRET;
+    const token = cookies.get('login');
 
-    const token = await platform.env.LOGIN_TOKENS.get(login);
-    console.log(token)
-    if (token === undefined) bail(cookies)
+    if (token === undefined || token !== secret) bail(cookies)
 }
 
 export const actions = {
     default: async ({ cookies, request, platform }) => {
         const formData = await request.formData();
-        const secret = formData.get('secret');
-        console.log(secret)
+        const token = formData.get('secret');
+        const secret = platform.env.SECRET;
 
-        if (!secret || secret === 'undefined')
-            return fail(400, {
-                error: true,
-                message: 'Secret is missing.'
-            });
+        if (token === undefined || token !== secret) bail(cookies)
 
-        if (secret !== platform.env.SECRET)
-            return fail (401, {
-                error: true,
-                message: 'Secret is not valid.'
-            });
-
-        const token = uuid();
-        const timestamp = new Date();
-
-        try {
-            await platform.env.LOGIN_TOKENS.put(token, timestamp.toISOString(), {expirationTtl: 3600});
-            cookies.set('login', token, {
-                path: '/',
-                httpOnly: true,
-                maxAge: 3600,
-                secure: true,
-                sameSite: true
-            });
-        }
-        catch {
-            return fail(500, {
-                error: true,
-                message: 'Failed to set login token.'
-            })
-        }
+        cookies.set('login', token, {
+            path: '/',
+            httpOnly: true,
+            maxAge: 3600,
+            secure: true,
+            sameSite: true
+        });
     }
 }
