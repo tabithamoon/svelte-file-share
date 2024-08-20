44c0568303338143bc87bce4039a292a1a664662
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2024-08-20 16:10:19 -0300

    Complete rewrite, switch to Axios

--- src/routes/api/upload/+server.js
+++ src/routes/api/upload/+server.js
@@ -1,118 +1,179 @@
 import { json } from '@sveltejs/kit';
 
 export async function POST({ request, platform }) {
-    const bucket = platform.env.MAIN_BUCKET
-    const secret = platform.env.SECRET
+    // Debug flag
+    const debug = true;
 
-    const fileName = request.headers.get('x-file-name')
-    const action = request.headers.get('x-upload-action')
-    const token = request.headers.get('authorization')
+    // Get Cloudflare bindings
+    const bucket = platform.env.MAIN_BUCKET;
+    const secret = platform.env.SECRET;
 
-    if (token !== secret) {
-        return json({ error: true, message: 'Unauthorized'}, { status: 401 })
-    }
+    // Create object to hold parameters for request
+    const command = {
+        token: request.headers.get('authorization'),
+        id: request.headers.get('x-upload-id'),
+        name: request.headers.get('x-file-name'),
+        expiry: request.headers.get('x-expire-time'),
+        action: request.headers.get('x-upload-action'),
+        address: request.headers.get('cf-connecting-ip')
+    };
 
-    if (fileName == null || fileName === '') {
+    if (debug) console.log(command)
+
+    // Bail if shared secret is not correct
+    if (command.token !== secret) {
         return json(
-            { error: true, message: 'File name not provided'},
-            { status: 400 }
-        )
+            { error: true, message: 'Unauthorized' },
+            { status: 401 }
+        );
     }
 
-    switch (action) {
-        case 'create':
-            try {
-                const mpUpload = await bucket.createMultipartUpload(fileName);
-                return json(
-                    {
-                        key: mpUpload.key,
-                        uploadId: mpUpload.uploadId
-                    }
-                )
-            }
-            catch (e) {
-                console.error(e);
-                return json(
-                    { error: true, message: 'Failed to create bucket upload.' },
-                    { status: 500 }
-                )
-            }
+    // Bail if the file name is invalid
+    if (command.name == null || command.name == '') {
+        return json(
+            { error: true, message: 'File name missing' },
+            { status: 400 }
+        );
+    }
 
-        case 'complete':
+    // Main logic, create or complete a multipart upload based on "X-Upload-Action" header
+    switch(command.action) {
+        case 'create': // Create a multipart upload
             try {
-                const uploadId = request.headers.get("x-upload-id");
-                const mpUpload = await bucket.resumeMultipartUpload(fileName, uploadId);
-
-                await mpUpload.complete(JSON.parse(request.body))
-            }
-            catch (e) {
-                console.error(e)
-                return (
-                    { error: true, message: 'Failed to complete multipart upload.' },
-                    { status: 500 }
+                // Create and immediately return multipart upload details
+                return json(
+                    await bucket.createMultipartUpload(
+                        command.name, {
+                            customMetadata: {
+                                "uploaderIp": command.address,
+                                "uploadDate": new Date().toISOString(),
+                                "expiryDate": command.expiry,
+                                "guest": "false" //TODO: implement guest links
+                            }
+                        }
+                    ),
+                    { status: 201 }
                 );
             }
-
-        default:
-            return json(
-                { error: true, message: 'Action not provided'}, 
-                { status: 400 }
-            )
-    }
-}
-
-export async function PUT({ request, platform }) {
-    const bucket = platform.env.MAIN_BUCKET
-    const secret = platform.env.SECRET
-
-    const fileName = request.headers.get('x-file-name')
-    const action = request.headers.get('x-upload-action')
-    const token = request.headers.get('authorization')
-    
-    console.log(token)
-    console.log(secret)
-    if (token !== secret) {
-        return json({ error: true, message: 'Unauthorized'}, { status: 401 })
-    }
-
-    if (fileName == null || fileName === '') {
-        return json(
-            { error: true, message: 'File name not provided'},
-            { status: 400 }
-        )
-    }
-
-    switch (action) {
-        case 'direct':
-            try {
-                return json(await bucket.put(fileName, request.body));
-            } catch (e) {
-                console.error(e)
+            catch (e) {
                 return json(
-                    { error: true, message: 'Failed to upload file.' },
+                    { error: true, message: e },
                     { status: 500 }
                 );
             }
         
-        case 'add-part':
+        case 'complete': // Complete a multipart upload
             try {
-                const uploadId = request.headers.get("x-upload-id");
-                const partNumber = request.headers.get("x-upload-part");
+                // Resume multipart upload
+                const upload = await bucket.resumeMultipartUpload(command.name, command.id);
 
-                const mpUpload = await bucket.resumeMultipartUpload(fileName, uploadId)
-                return json(await mpUpload.uploadPart(partNumber, request.body));
-            } catch (e) {
-                console.error(e)
+                // Return completed upload details
                 return json(
-                    { error: true, message: 'Failed to upload part'},
+                    await upload.complete(await request.json())
+                );
+            }
+            catch (e) {
+                return json(
+                    { error: true, message: e },
                     { status: 500 }
                 );
             }
 
-        default:
+        default: // Return an error if there is no action defined
             return json(
-                { error: true, message: 'Action not provided'}, 
+                { error: true, message: 'No action provided' },
                 { status: 400 }
-            )
+            );
+    }
+}
+
+export async function PUT({ request, platform, event }) {
+    // Debug flag
+    const debug = true;
+
+    // Get Cloudflare bindings
+    const bucket = platform.env.MAIN_BUCKET;
+    const secret = platform.env.SECRET;
+
+    // Create object to hold parameters for request
+    const command = {
+        token: request.headers.get('authorization'),
+        id: request.headers.get('x-upload-id'),
+        name: request.headers.get('x-file-name'),
+        part: request.headers.get('x-upload-part'),
+        expiry: request.headers.get('x-expire-time'),
+        action: request.headers.get('x-upload-action'),
+        address: request.headers.get('cf-connecting-ip')
+    };
+
+    if (debug) console.log(command);
+
+    // Bail if shared secret is not correct
+    if (command.token !== secret) {
+        return json(
+            { error: true, message: 'Unauthorized' },
+            { status: 401 }
+        );
+    }
+
+    // Bail if the file name is invalid
+    if (command.name == null || command.name == '') {
+        return json(
+            { error: true, message: 'File name missing' },
+            { status: 400 }
+        );
+    }
+
+    // Main logic, switch between single request or chunked upload based on "X-Upload-Action" header
+    switch (command.action) {
+        case 'direct': // <96MB upload, send in single request
+            try {
+                // Directly place file in bucket in a single chunk
+                return json(
+                    await bucket.put(
+                        command.name, // File name
+                        request.body, // File data
+                        {
+                            // Add info about the upload attached to object itself
+                            customMetadata: {
+                                "uploaderIp": command.address,
+                                "uploadDate": new Date().toISOString(),
+                                "expiryDate": command.expiry,
+                                "guest": "false" // TODO: implement guest links
+                            }
+                        }
+                    ),
+                    { status: 201 }
+                );
+            }
+            catch (e) {
+                return json(
+                    { error: true, message: e },
+                    { status: 500 }
+                );
+            }
+        
+        case 'add-part': // Add part to existing multipart upload
+            try {
+                // Resume ongoing chunked upload
+                const upload = await bucket.resumeMultipartUpload(command.name, command.id);
+
+                // Add new part to upload
+                return json(
+                    await upload.uploadPart(command.part, request.body)
+                );
+            }
+            catch (e) {
+                return json(
+                    { error: true, message: e },
+                    { status: 500 }
+                );
+            }
+        
+        default: // Return an error if there is no action defined
+            return json(
+                { error: true, message: 'No action provided' },
+                { status: 400 }
+            );
     }
 }
--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -1,88 +1,162 @@
 <script>
-    export let data;
-    let files;
+    // Imports
+	import axios from 'axios';
 
+    let files;                          // selected files to upload
+    export let data;                    // data from backend
+    export let uploadProgress;          // user facing upload progress percentage
+
+    // Main upload function
     const uploadFiles = async () => {
-        const chunkSize = 100663296 // 96MB (in bytes)
-        const token = data.token
-        debugger;
+        // Static vars
+        const chunkSize = 100663296;    // 96MB (in bytes)
+        const token = data.token;       // Auth token (from cookies)
+        const debug = true;             // Enable debug logging in console
+        
+        // A unified "response" variable, for API requests
+        let response = undefined;
+
+        // TODO: stub, implement setting expiry date
+        let expiryDate = new Date();
+        expiryDate.setTime(expiryDate.getTime() + 86400000); // 1 day (in ms)
+        if (debug) console.log(`Upload expiry time: ${expiryDate.toISOString()}`);
+
+        // Set some "unified" headers that are
+        // shared between chunked and standard upload
+        let headers = {
+            "Authorization": token,
+            "X-Expire-Time": expiryDate.toISOString()
+        }
+
+        // Iterate over every selected file
         for (const file of files) {
-            console.log(file);
+            if (debug) console.log(file);
+
+            // Set file name for this upload
+            headers["X-File-Name"] = file.name;
+
+            // Use multipart upload if file size > chunk size
             if (file.size > chunkSize) {
-                console.log("chunked upload");
-                
-                let start = 0;
-                let partNumber = 1;
-                let upload = undefined;
-                const headers = new Headers();
+                if (debug) console.log(`File size (${file.size}) larger than chunk size (${chunkSize}), using chunked upload`);
 
-                // Set unified headers
-                headers.set("Authorization", token);
-                headers.set("X-File-Name", "1d/" + file.name);
+                // Set upload action to create multipart upload
+                headers["X-Upload-Action"] = "create";
 
-                // Create multipart upload
-                headers.set("X-Upload-Action", "create");
-                try {
-                    upload = JSON.parse(await fetch("/api/upload", {
-                        method: 'POST',
+                // Create a multipart upload
+                response = await axios.post(
+                    "/api/upload",
+                    null,
+                    {
                         headers: headers
-                    }));
-                }
-                catch (e) {
-                    console.error(`Failed to connect to API: ${e}`);
-                }
+                    }
+                );
 
-                if (upload.error) {
-                    console.error(upload.message);
-                    return
+                // End early if response is not Created
+                if (response.status !== 201) {
+                    console.error(`Failed to create multipart upload: ${response.data}`);
+                    return;
                 }
 
-                // Set upload ID header and add-part action
-                headers.set("X-Upload-Id", upload.uploadId);
-                headers.set("X-Upload-Action", "add-part");
+                // Discard rest of response info to simplify
+                response = response.data;
+                if (debug) console.log(`Multipart upload created: ${response}`);
 
-                // Main upload loop
-                let uploadedParts = [];
-                while (start < file.size) {
-                    console.log(`uploading chunk ${partNumber}`);
+                
+                headers["X-Upload-Action"] = "add-part"; // Set upload action to add part to chunked upload
+                headers["X-Upload-Id"] = response.uploadId; // Set upload ID header
 
-                    headers.set("X-Upload-Part", partNumber);
+                let part = 1;           // Current part number
+                let offset = 0;         // Current offset from file start
+                let partsList = [];     // Keep track of list of parts to complete upload 
 
-                    uploadedParts.append(JSON.parse(await fetch("/api/upload", {
-                        method: 'PUT',
-                        headers: headers,
-                        body: file.slice(start, start + chunkSize)
-                    })));
-                    start += chunkSize;
-                    partNumber++;
+                // Main chunked upload loop
+                // Until the offset moves beyond the end of the file...
+                while (offset < file.size) {
+                    if (debug) console.log(`Uploading chunk ${part}, range ${offset} -> ${offset + chunkSize}`);
+                    
+                    // Set current chunk header
+                    headers["X-Upload-Part"] = part;
+
+                    // Upload chunk request
+                    response = await axios.put(
+                        '/api/upload',
+                        file.slice(offset, offset + chunkSize),
+                        {
+                            headers: headers,
+                            onUploadProgress: (progressEvent) => {
+                                const { loaded } = progressEvent;
+                                uploadProgress = Math.floor(((offset + loaded) * 100) / file.size);
+                                if (debug) console.log(`Upload progress: ${(offset + loaded)} out of ${file.size}`);
+                            }
+                        }
+                    );
+
+                    if (response.status !== 200) {
+                        console.error(`Uploading file part error: ${response.data}`);
+                        return;
+                    }
+
+                    partsList.push(response.data);  // Add R2UploadedPart object to array to complete upload later
+                    offset = offset + chunkSize;    // Set new offset after upload completed
+                    part++;                         // Increment part number
                 }
+
+                // Set upload action to complete multipart upload
+                headers["X-Upload-Action"] = "complete";
 
                 // Complete multipart upload
-                headers.set("X-Upload-Action", "complete");
-                let completeResult = JSON.parse(await fetch("/api/upload", {
-                    method: 'POST',
-                    headers: headers,
-                    body: JSON.stringify(uploadedParts)
-                }))
+                response = await axios.post(
+                    '/api/upload',
+                    partsList,
+                    {
+                        headers: headers
+                    }
+                )
+
+                // Error out if response is not OK
+                if (response.status !== 200) {
+                    console.error(`Failed to complete multipart upload: ${response.data}`);
+                    return;
+                }
             }
+
+            // File size < chunk size, send in one request
             else {
-                console.log("direct upload");
+                if (debug) console.log(`File size (${file.size}) smaller than chunk size (${chunkSize}), using single request upload`);
 
-                const headers = new Headers();
-                headers.set("Authorization", token);
-                headers.set("X-File-Name", "1d/" + file.name);
-                headers.set("X-Upload-Action", "direct");
+                // Set upload action to single request upload
+                headers["X-Upload-Action"] = "direct";
+                
+                if (debug) console.log(headers);
 
-                const result = await fetch('/api/upload', {
-                    method: 'PUT',
-                    body: file,
-                    headers: headers
-                });
+                // Make upload
+                response = await axios.put(
+                    "/api/upload",
+                    file,
+                    {
+                        headers: headers,
+                        onUploadProgress: (progressEvent) => {
+                            const { loaded, total } = progressEvent;
+                            uploadProgress = Math.floor((loaded * 100) / total);
+                            if (debug) console.log(`Upload progress: ${loaded} out of ${total}`);
+                        },
+                    }
+                );
 
-                console.log(result);
+                if (debug) console.log(response);
+
+                // Error out if status is not Created
+                if (response.status !== 201) {
+                    console.error(`Upload failed: ${response.data}`);
+                    return;
+                }
             }
         }
+
+        // TODO: (better) user UI confirmation
+        alert("upload success!");
     }
 </script>
 
 <input type="file" bind:files on:change={uploadFiles}/>
+<p>{uploadProgress}</p>
