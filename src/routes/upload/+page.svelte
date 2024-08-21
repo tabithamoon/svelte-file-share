79d5472add50a69b9516fd903ca16b078ce50795
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2024-08-21 11:35:00 -0300

    Switched kibibytes to kilobytes I know this is wrong but... Windows gets it wrong too and I'm lazy.

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -17,7 +17,7 @@
     // Credit to https://stackoverflow.com/a/42408230
     function shortenBytes(n) {
         const k = n > 0 ? Math.floor((Math.log2(n)/10)) : 0;
-        const rank = (k > 0 ? 'KMGT'[k - 1] : '') + 'iB';
+        const rank = (k > 0 ? 'KMGT'[k - 1] : '') + 'B';
         const count = Math.floor(n / Math.pow(1024, k));
         return count + rank;
     }
