d5708e5772023f8503c96456a434419fe3e30a68
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2024-08-22 09:47:41 -0300

    Added logout query string to clear cookie

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,20 @@
 import { redirect } from '@sveltejs/kit';
 
-export function load({ cookies }) {
+export function load({ cookies, url }) {
     const login = cookies.get('login');
-    if (login !== undefined) redirect(307, '/upload')
+    const logout = url.searchParams.get('logout');
+
+    console.log(logout)
+
+    if (logout != null) {
+        cookies.set('login', '', {
+            path: '/',
+            httpOnly: true,
+            maxAge: 0,
+            secure: true,
+            sameSite: true
+        });
+        return;
+    }
+    if (login != null && login != '') redirect(307, '/upload')
 }
